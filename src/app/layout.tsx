5060f9b9ee9c5a578c7a658bd774a6024a2ff0ed
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-03-25 22:27:57 +0700

    feat: coming-soon

---
+++ src/app/layout.tsx
@@ -0,0 +1,99 @@
+import "@/styles/globals.css";
+
+import { Montserrat as FontSans } from "next/font/google";
+import localFont from "next/font/local";
+// import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
+import { cn } from "@/lib/utils";
+import { TRPCReactProvider } from "@/trpc/react";
+import { Footer } from "@/components/common/footer";
+import { GlobalFooter } from "@/components/common/global-footer";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+const fontSerif = localFont({
+  src: [
+    {
+      path: "./fonts/minion-pro/MinionPro-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/minion-pro/MinionPro-It.otf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./fonts/minion-pro/MinionPro-Medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/minion-pro/MinionPro-MediumIt.otf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "./fonts/minion-pro/MinionPro-SemiBold.otf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./fonts/minion-pro/MinionPro-SemiBoldIt.otf",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "./fonts/minion-pro/MinionPro-Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/minion-pro/MinionPro-BoldIt.otf",
+      weight: "700",
+      style: "italic",
+    },
+  ],
+  variable: "--font-serif",
+});
+
+export const metadata = {
+  title: "HIMARPL",
+  description: "Masih dalam tahap pengembangan!",
+  icons: [
+    {
+      rel: "icon",
+      url: "/favicon.ico",
+    },
+  ],
+};
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="scroll-smooth">
+      {/* <GoogleTagManager gtmId="GTM-XYZ" /> */}
+
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+          fontSerif.variable,
+        )}
+      >
+        <TRPCReactProvider>
+          {children}
+          <Footer />
+          <GlobalFooter />
+        </TRPCReactProvider>
+      </body>
+
+      {/* <GoogleAnalytics gaId="G-XYZ" /> */}
+    </html>
+  );
+}
