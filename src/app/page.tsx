3de428bf789f23ec2185cb98048d99dd513a5b51
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-07-01 08:09:32 +0700

    chore: remove lomba poster dies natalis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 import { LogoAscendia } from "@/components/brand/logo-ascendia";
 import { ThemeModeToggle } from "@/components/common/theme-mode-toggle";
 import { ComingSoon } from "@/components/home/coming-soon";
@@ -8,13 +6,6 @@ export default function Home() {
   return (
     <>
       <nav className="container space-x-2 bg-secondary/50 py-2 text-center">
-        <Link
-          href="https://lomba.himarpl.com/"
-          className="underline transition hover:text-foreground/75"
-        >
-          Ikuti Lomba Poster Dies Natalis RPL ke-5 Disini! Gratis Loh!
-        </Link>
-
         <ThemeModeToggle />
       </nav>
       <main className="container flex min-h-screen flex-col items-center justify-center py-8">
